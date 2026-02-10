ded7be4f049c57279994123147fbea25a5104204
dionnell <dionnellr1@gmail.com>
2026-02-09 21:23:30 -0300

    feat: enhance header and footer with custom logo, improve search functionality, and update gender page layout

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,7 +1,29 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useParams } from "react-router"
 
 
 export const GenderPage = () => {
+
+  const {gender} = useParams()
+  const genderTitle = gender === 'men'
+    ? 'Hombres'
+    : gender === 'women'
+      ? 'Mujeres'
+      : 'Niños'
+
   return (
-    <div>GenderPage</div>
+    <>
+          <CustomJumbotron 
+          title={`Productos para ${genderTitle}`}
+          subtitle="Explora nuestra colección completa de productos premium." 
+          />
+    
+          <ProductsGrid products={products} />
+    
+          <CustomPagination totalPages={5} />
+        </>
   )
 }
