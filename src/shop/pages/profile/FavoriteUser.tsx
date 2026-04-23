2a391fa498b563020e7f441dd41c30380853a2c5
dionnell <dionnellr1@gmail.com>
2026-04-23 16:50:37 -0400

    refactor: adjust margin for product info layout in FavoriteUser component

--- src/shop/pages/profile/FavoriteUser.tsx
+++ src/shop/pages/profile/FavoriteUser.tsx
@@ -119,7 +119,7 @@ export const FavoriteUser = () => {
                   </Link>
 
                   {/* Info */}
-                  <div className="flex-1 min-w-0 flex flex-col justify-center gap-1 max-sm:col-span-2">
+                  <div className="flex-1 min-w-0 flex flex-col justify-center gap-1 max-sm:col-span-2 max-sm:ml-2">
                     <Link
                       to={`/product/${favorite.product.slug}`}
                       className="text-sm font-semibold text-gray-900 line-clamp-2 hover:cursor-pointer hover:text-blue-600 transition-colors duration-200"
