cc1ca22a72b35053ad366b67b8ad641567d945a2
dionnell <dionnellr1@gmail.com>
2026-05-04 14:58:42 -0400

    feat: adjust grid gap for improved layout in AdminProductForm

--- src/admin/pages/product/ui/AdminProductForm.tsx
+++ src/admin/pages/product/ui/AdminProductForm.tsx
@@ -136,8 +136,8 @@ export const AdminProductForm = ({ title, subTitle, product, onSubmit, isPosting
         </div>
       </div>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
           {/* Main Form */}
           <div className="lg:col-span-2 space-y-6">
             {/* Basic Information */}
